44a54e9e1cb07f32384a0544744e58c8552672f2
Gabriel Bertholin <52984793+ghol1n@users.noreply.github.com>
2022-08-30 21:47:42 -0300

    Create aula4.r

---
+++ aula 4/aula4.r
@@ -0,0 +1,25 @@
+
+install.packages("dplyr")
+install.packages("plyr")
+install.packages("tidyverse")
+
+
+
+library(plyr)
+library(dplyr)
+library(tidyverse)
+
+
+municipios%>%
+  filter(uf_code == "SP")
+
+
+
+municipios%>%
+  arrange(., desc(area))
+
+
+municipios%>%
+  filter(mesoregion == 1102)
+
+
